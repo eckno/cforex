01efaeb3207a3ca8ddfda0616162a5c36a5dc082
techbros <jessefamous29@gmail.com>
2023-04-05 01:24:34 +0100

    page setup

--- routes/account/index.js
+++ routes/account/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const IndexController = require("../../controllers");
+const indexController = new IndexController();
+
 router.get("/", async (req, res) => {
-    console.log("hi");
-    //return indexController.loginAction(req, res);
+    return indexController.loginAction(req, res);
+});
+
+router.get("/register", async (req, res) => {
+    return indexController.registerAction(req, res);
+});
+
+router.get("/reset", async (req, res) => {
+    return indexController.resetAction(req, res);
 });
 
 
